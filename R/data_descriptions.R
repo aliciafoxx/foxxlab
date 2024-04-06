054c342a7c4c0e22feb8628dd6c6a530f1416a0c
Alicia Foxx <afoxx@u.northwestern.edu>
2024-04-06 05:33:33 -0500

    added data descriptions

--- R/data_descriptions.R
+++ R/data_descriptions.R
@@ -68,5 +68,107 @@
 #'   head(fk2020a)
 #' }
 
-
 "fk2020a"
+
+#' Dataset Description
+#'
+#' This dataset contains information about from the fk2020b publication.
+#'
+#' @format A data frame with 180 rows and 15 variables:
+#'   \describe{
+#'     \item{\code{No.}}{Numeric vector representing the observation number.}
+#'     \item{\code{Species}}{Character vector representing the species name.}
+#'     \item{\code{Accession}}{Character vector representing the accession ID.}
+#'     \item{\code{Source_Accession_Info}}{Character vector representing information about the source accession.}
+#'     \item{\code{Collection_type}}{Character vector representing the collection type.}
+#'     \item{\code{Block}}{Character vector representing the block information.}
+#'     \item{\code{Date_Planted}}{Character vector representing the date planted.}
+#'     \item{\code{Date_Harvested}}{Character vector representing the date harvested.}
+#'     \item{\code{Age_at_harvest}}{Numeric vector representing the age at harvest.}
+#'     \item{\code{Root_length_.cm.}}{Numeric vector representing the root length in centimeters.}
+#'     \item{\code{Height_.mm.}}{Numeric vector representing the height in millimeters.}
+#'     \item{\code{No_of_leaves}}{Numeric vector representing the number of leaves.}
+#'     \item{\code{Root_mass_.mg.}}{Numeric vector representing the root mass in milligrams.}
+#'     \item{\code{Shoot_mass_.mg.}}{Numeric vector representing the shoot mass in milligrams.}
+#'     \item{\code{No_of_Root_tips}}{Numeric vector representing the number of root tips.}
+#'   }
+#'
+#' @source https://onlinelibrary.wiley.com/doi/abs/10.1111/rec.13336
+#'
+#' @examples
+#' \dontrun{
+#'   head(fk2020b)
+#' }
+
+"fk2020b"
+
+
+#' Dataset Description
+#'
+#' This dataset contains information from the f2021 dataset.
+#'
+#' @format A data frame with 87 rows and 17 variables:
+#'   \describe{
+#'     \item{\code{No}}{Numeric vector representing the observation number.}
+#'     \item{\code{Focal_Species}}{Character vector representing the focal species.}
+#'     \item{\code{Non-focal_plant}}{Character vector representing the non-focal plant species.}
+#'     \item{\code{Treatment}}{Character vector representing the treatment applied.}
+#'     \item{\code{Exp}}{Character vector representing the experimental condition.}
+#'     \item{\code{No_of_Root_Tips}}{Numeric vector representing the number of root tips.}
+#'     \item{\code{Total_Mass_Exp_1}}{Numeric vector representing the total mass for Experiment 1.}
+#'     \item{\code{Root_Mass_Exp_1}}{Numeric vector representing the root mass for Experiment 1.}
+#'     \item{\code{Shoot_Mass_Exp_1}}{Numeric vector representing the shoot mass for Experiment 1.}
+#'     \item{\code{Plant_Height(mm)_Exp_1}}{Numeric vector representing the plant height in millimeters for Experiment 1.}
+#'     \item{\code{RMF_Exp_1}}{Numeric vector representing the root mass fraction for Experiment 1.}
+#'     \item{\code{Community_Rhizome_Exp_2}}{Numeric vector representing the community rhizome for Experiment 2.}
+#'     \item{\code{Notes}}{Logical vector indicating the presence of notes.}
+#'     \item{\code{All_Focal_Shoot_Mass}}{Numeric vector representing the shoot mass for all focal plants.}
+#'     \item{\code{focal_leaf_count}}{Numeric vector representing the leaf count for focal plants.}
+#'     \item{\code{nf_leaf_count_1}}{Numeric vector representing the leaf count for non-focal plants (1st count).}
+#'     \item{\code{nf_leaf_count_2}}{Numeric vector representing the leaf count for non-focal plants (2nd count).}
+#'   }
+#'
+#' @source [https://www.nature.com/articles/s41598-021-93494-0]
+#'
+#' @examples
+#' \dontrun{
+#'   head(f2021)
+#' }
+
+"f2021"
+
+#' Dataset Description
+#'
+#' This dataset contains information from the f2022 dataset.
+#'
+#' @format A data frame with 483 rows and 18 variables:
+#'   \describe{
+#'     \item{\code{No}}{Numeric vector representing the observation number.}
+#'     \item{\code{Tube_No}}{Numeric vector representing the tube number.}
+#'     \item{\code{Species_one}}{Character vector representing the first species.}
+#'     \item{\code{Species_two...4}}{Character vector representing the second species.}
+#'     \item{\code{Block}}{Character vector representing the block.}
+#'     \item{\code{Location}}{Character vector representing the location.}
+#'     \item{\code{Treatment}}{Character vector representing the treatment.}
+#'     \item{\code{Planting_date}}{Character vector representing the planting date.}
+#'     \item{\code{Surv_final}}{Numeric vector representing the final survival status.}
+#'     \item{\code{sp1_Abovgd_mass}}{Numeric vector representing the aboveground mass for species one.}
+#'     \item{\code{Community_root_mass}}{Numeric vector representing the community root mass.}
+#'     \item{\code{sp1_Total_Mass}}{Numeric vector representing the total mass for species one.}
+#'     \item{\code{Comm_RMF}}{Numeric vector representing the community root mass fraction.}
+#'     \item{\code{No_of_Plants}}{Numeric vector representing the number of plants.}
+#'     \item{\code{Control, Intra, Inter}}{Character vector representing the control, intra, or inter status.}
+#'     \item{\code{Species_two...16}}{Character vector representing the second species (continued).}
+#'     \item{\code{sp2_Abovgd_mass}}{Numeric vector representing the aboveground mass for species two.}
+#'     \item{\code{sp2_Total_Mass}}{Numeric vector representing the total mass for species two.}
+#'   }
+#'
+#' @source [https://sciendo.com/article/10.2478/foecol-2022-0019]
+#'
+#' @examples
+#' \dontrun{
+#'   head(f2022)
+#' }
+
+"f2022"
+
