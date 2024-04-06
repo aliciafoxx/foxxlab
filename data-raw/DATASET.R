27cb270b51c1951f49bc11b55c054b696d7e95fc
Alicia Foxx <afoxx@u.northwestern.edu>
2024-04-06 04:08:51 -0500

    added data files to the package

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,9 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+
+library(tidyverse)
+
+fk2020a <-
+  readr::read_csv("G:\\My Drive\\1. Research + resources\\Workshops + Resources\\foxxlab package\\foxxlab\\data-raw\\Foxx_&_Kramer_2020a.csv")
+
+usethis::use_data(fk2020a, overwrite = TRUE)
