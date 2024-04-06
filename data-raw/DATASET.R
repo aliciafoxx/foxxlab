cf4ec3cf53d61d6e017e58e057f5cbeb6584d869
Alicia Foxx <afoxx@u.northwestern.edu>
2024-04-06 04:19:46 -0500

    updated data files

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,7 +3,32 @@
 
 library(tidyverse)
 
+# 2020 MS publication
 fk2020a <-
-  readr::read_csv("G:\\My Drive\\1. Research + resources\\Workshops + Resources\\foxxlab package\\foxxlab\\data-raw\\Foxx_&_Kramer_2020a.csv")
+  readr::read_csv(readr::read_csv(here::here("data-raw", "Foxx_&_Kramer_2020a.csv")))
 
 usethis::use_data(fk2020a, overwrite = TRUE)
+
+# 2020 dissertation publication
+fk2020b <-
+  readr::read_csv(readr::read_csv(here::here("data-raw", "Foxx_&_Kramer_2020b.csv")))
+
+usethis::use_data(fk2020b, overwrite = TRUE)
+
+# 2021 publication
+f2021 <-
+  readr::read_csv(readr::read_csv(here::here("data-raw", "Foxx_2021.csv")))
+
+usethis::use_data(f2021, overwrite = TRUE)
+
+# 2022 publication
+f2022 <-
+  readr::read_csv(readr::read_csv(here::here("data-raw", "Foxx_2022.csv")))
+
+usethis::use_data(f2022, overwrite = TRUE)
+
+# 2024 dissertation chapter manuscript
+fvk2024 <-
+  readr::read_csv(readr::read_csv(here::here("data-raw", "FVK_2023.csv")))
+
+usethis::use_data(fvk2024, overwrite = TRUE)
